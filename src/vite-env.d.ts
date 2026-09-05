aa734dffce570776adf380ec67cffc931083f360
Rawsie <rawsienguyen@gmail.com>
2026-09-06 02:40:33 +0700

    Publish version

---
+++ src/vite-env.d.ts
@@ -0,0 +1,11 @@
+/// <reference types="vite/client" />
+
+declare module '*.mid' {
+	const src: string;
+	export default src;
+}
+
+declare module '*.midi' {
+	const src: string;
+	export default src;
+}
