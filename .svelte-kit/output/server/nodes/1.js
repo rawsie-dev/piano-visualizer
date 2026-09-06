5457aeb088e92c1d06ce045eab568f1aa073e846
Rawsie <rawsienguyen@gmail.com>
2026-09-06 17:20:29 +0700

    rebuild + workflow

--- .svelte-kit/output/server/nodes/1.js
+++ .svelte-kit/output/server/nodes/1.js
@@ -3,6 +3,6 @@
 export const index = 1;
 let component_cache;
 export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.DLXpGD-j.js","_app/immutable/chunks/DODMdrAD.js","_app/immutable/chunks/BioAEALW.js","_app/immutable/chunks/xihTtKlq.js"];
+export const imports = ["_app/immutable/nodes/1.CmRr85l1.js","_app/immutable/chunks/DODMdrAD.js","_app/immutable/chunks/CgZ-KmsT.js","_app/immutable/chunks/xihTtKlq.js"];
 export const stylesheets = [];
 export const fonts = [];
--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,15 @@
 import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-const config = {
-	kit: {
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build'
-		})
-	}
+export default {
+  preprocess: vitePreprocess(),
+  kit: {
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: '404.html',
+      precompress: false,
+      strict: true
+    })
+  }
 };
-
-export default config;
